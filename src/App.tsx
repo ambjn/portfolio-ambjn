fa86cd43ab7280982a0b08b7c857486fb0e03c8c
Amber Jain <jainanuamber@gmail.com>
2026-01-23 20:03:21 +0530

    imported in app main

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Bio from "./components/Bio";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="max-w-4xl mx-auto px-4 sm:px-6 py-8 sm:py-12">
+      <main>
+        <Bio />
+        <Projects />
+        <Work />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
